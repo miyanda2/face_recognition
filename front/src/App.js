e133bc0ed808890f7d7f44134b0c12e62d6ca77d
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-02-07 19:40:27 +0200

    init react project, created primary version of gallery and added emotions

---
+++ front/src/App.js
@@ -0,0 +1,57 @@
+// external
+import React, { Component, Fragment } from 'react';
+// internal
+import Emotions from '../src/components/Emotions/Emotions';
+import Spinner from '../src/components/Spinner/Spinner';
+import GalleryBuilder from './containers/GalleryBuilder/GalleryBuilder';
+import axios from './axios';
+// style
+import './App.css';
+
+
+class App extends Component {
+    state = {
+        isLoading: true,
+        photos: []
+    }
+
+    componentDidMount() {
+        const tag = '',
+        apiKey = '4a2173dec440a23318af07a5354b082f',
+        albumId = 72157674388093532,
+        userId = '144522605@N06'
+
+        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&photoset_id=${albumId}&user_id=${userId}&tags=${tag}&format=json&nojsoncallback=1`)
+            .then(response => {
+                const photos = response.data.photos.photo,
+                    photoURLs = [];
+
+                photos.forEach(photo => {
+                    const photoURL = `http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+                    photoURLs.push(photoURL);
+                });
+
+                this.setState({
+                    isLoading: false,
+                    photos: photoURLs
+                });
+            });
+    }
+
+  render() {
+    let content = <Spinner/>;
+
+    if (!this.state.isLoading) {
+        content = (
+            <Fragment>
+                <Emotions/>
+                <GalleryBuilder photos={this.state.photos} />
+            </Fragment>
+        );
+    }
+
+    return content;
+  }
+}
+
+export default App;
